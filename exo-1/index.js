5114610f7932d448c07d4a202552d952f3ef1feb
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-02-07 09:00:00 +0700

    Iteration-2

--- exo-1/index.js
+++ exo-1/index.js
@@ -1,2 +1,17 @@
 let elem = document.getElementById('fill-me')
 elem.innerHTML = 'HAlo!'
+
+let pTags = document.getElementsByClassName('change-all-of-me')
+// console.log(pTags, 'is pTags an array???')
+// console.log(typeof pTags)
+// pTags.forEach(currentItem => {})(pTag => {
+//   return (pTag.innerHTML = 'HALO JUga!')
+// })
+
+// for (let key in pTags) {
+//   console.log(key)
+// }
+
+for (let i = 0; i < pTags.length; i++) {
+  pTags[i].innerHTML = 'HALO JUga!'
+}
